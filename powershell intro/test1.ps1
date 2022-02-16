713a24e5c592e72dae0c12bd833afaeae670ed4d
wardgodd <ward.goddefroy@gmail.com>
2022-02-16 16:07:35 +0100

    day 1

---
+++ powershell intro/test1.ps1
@@ -0,0 +1,21 @@
+notepad
+get-process -Name notepad
+Stop-Process -Name notepad -WhatIf
+
+
+show-command get-process 
+
+
+get-service -name win*
+
+get-service -name w?nrm
+
+get-service -name [VX]*
+get-service -name [VWX]*
+
+
+get-service | 
+get-service
+
+
+
